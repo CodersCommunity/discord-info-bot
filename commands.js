cbb71d357913af170c2469fe617f5e7dcb9fa3b5
Arkadiusz Waluk <arkadiusz@waluk.pl>
2022-10-29 00:53:33 +0200

    Commands - update 'pytanie', add 'nick', 'priv', 'everyone'

--- commands.js
+++ commands.js
@@ -13,10 +13,22 @@ module.exports = {
     },
     "pytanie": {
         "title": "Zadaj konkretne pytanie!",
-        "content": "Nie pytaj, czy możesz zapytać albo czy znajdzie się specjalista z danej dziedziny. Po prostu zadaj właściwe pytanie, czyli opisz szczegółowo swój problem, wstaw kod, pokaż ewentualne błędy. Jeżeli ktoś będzie wiedział, to na pewno spróbuje Ci pomóc.\n_[Don't ask to ask, just ask](https://dontasktoask.com)_"
+        "content": "Nie pytaj, czy możesz zapytać albo czy znajdzie się specjalista z danej dziedziny. Po prostu zadaj właściwe pytanie, czyli opisz szczegółowo swój problem, wstaw kod, pokaż ewentualne błędy. Jeżeli ktoś będzie wiedział, to na pewno spróbuje Ci pomóc.\n_[Jak brzmi pytanie?](https://jakbrzmipytanie.pl/)_ | _[Don't ask to ask, just ask](https://dontasktoask.com)_"
     },
     "gotowce": {
         "title": "Nie dajemy gotowców",
         "content": "Wybacz, ale nikt nie odrobi Twojego zadania dla Ciebie, jeżeli sam nie masz ochoty zrobić nic. Nawet gdyby tak się stało, to problem powróci już przy kolejnym zadaniu - otrzymując tylko rozwiązanie nadal się niczego nie nauczysz.\n\nZaangażuj się, poszukaj, spróbuj sam. Zapytaj konkretnie czego nie wiesz i powiedz jak możemy Ci pomóc, a wtedy pewnie ktoś spróbuje doradzić."
+    },
+    "nick": {
+        "title": "Zmiana nicka",
+        "content": "Na naszym serwerze nie możesz samodzielnie dokonać dowolnej zmiany nicka. Wszyscy mamy tutaj takie nazwy, jak nazwa użytkownika na forum. Możesz jednak łatwo dokonać jej zmiany na [stronie edycji profilu](https://forum.pasja-informatyki.pl/account). Zmiana możliwa jest raz na 7 dni, na Discordzie zostanie od razu wykonana automatycznie. Dzięki temu rozwiązaniu w obydwu miejscach panuje porządek - można jasno określić kto jest kim."
+    },
+    "priv": {
+        "title": "Pomoc przez wiadomości prywatne",
+        "content": "Rozumiemy, że czasem może wydawać się, że rozwiązanie problemu przez rozmowę prywatną jest łatwiejsze, ale prosimy o **zadawanie pytań publicznie**. Tym sposobem przede wszystkim zwiększasz swoją szansę na otrzymanie pomocy, ponieważ pytając prywatnie jednej osoby dotrze to tylko do niej, a nie wielu osób z których każdy może mieć inną wiedzę i pomysły. Nie każdy może mieć też czas i ochotę na prywatne rozmowy w danym momencie.\n\nOpisz dokładnie swoje pytanie na właściwym kanale i cierpliwie poczekaj na odpowiedź, gdy tylko ktoś będzie mógł Ci pomóc na pewno to zrobi."
+    },
+    "everyone": {
+        "title": "Nie pinguj wszystkich",
+        "content": "Nie próbuj wołać wszystkich użytkowników do swojego problemu! Wywołanie tylu osób tylko po to, aby wszyscy przeczytali akurat Twoje pytanie nie jest wcale miłe. Nie każdy ma akurat czas, możliwości czy wiedzę, aby udzielić odpowiedzi. Wystarczy, że opiszesz dokładnie swój problem i poczekasz na odpowiedź."
     }
 };
